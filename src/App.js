04673cc605f9b7a806fb62d53641a8a546efb7ae
Vairamuthu <mvairamuthu2003@gmail.com>
2022-11-09 13:03:25 +0530

    updated the element componenets

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Banners from "./Components/Elements/Banners";
+import Headers from "./Components/Elements/Headers";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Headers />
+      <Banners />
+    </div>
+  );
 }
 
 export default App;
