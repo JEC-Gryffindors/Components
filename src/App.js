685777b8cc9b7dc57075fcd4dcb56016080e1e3e
Vairamuthu <mvairamuthu2003@gmail.com>
2022-11-09 12:51:31 +0530

    updating the hero component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Hero from "./Components/PageSection/Hero";
+
+function App() {
+  return (
+    <div className="App">
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
