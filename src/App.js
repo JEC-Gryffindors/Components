7f31427bf4ea8ebe0fb36eb4b0060acf843805c7
Vairamuthu <mvairamuthu2003@gmail.com>
2022-11-09 13:37:26 +0530

    updating the overlay methods

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import DescriptionList from "./Components/Elements/DescriptionList";
+import FormLayouts from "./Components/Elements/FormLayouts";
+import Forms from "./Components/Elements/Forms";
+import Navbars from "./Components/Elements/Navbars";
+import Pagination from "./Components/Elements/Pagination";
+import SelectMenu from "./Components/Elements/SelectMenu";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Pagination />
+    </div>
+  );
 }
 
 export default App;
